e86c6fae9008fa95ac4779f837076c2477d2ea98
Broomhilda Von Shaft <30404027+akimbaev@users.noreply.github.com>
2020-06-23 23:37:26 +0200

    a little changes

--- front.py
+++ front.py
@@ -6,7 +6,6 @@ from tkinter import messagebox as ms
 from PIL import ImageTk, Image
 import sqlite3
 from frontend import Window
-# make database and users (if not exists already) table at programme start up
 with sqlite3.connect('quit.db') as db:
     c = db.cursor()
 
@@ -17,9 +16,7 @@ class main:
     def __init__(self,master):
         self.n_username = StringVar()
         self.n_password = StringVar()
-        # Window
         self.master = master
-        # Some Usefull variables
         self.username = StringVar()
         self.password = StringVar()
         #Create Widgets
@@ -35,16 +32,14 @@ class main:
         result = c.fetchall()
         if result:
             self.logf.pack_forget()
-            self.head['text'] = self.username.get() + '\n Loged In'
+            self.head['text'] = self.username.get() + '\n Enjoy your Time'
             self.head['pady'] = 150
-            showinfo(title = "success", message = "Username and password correct")
-    		# self.destroy()
-    	    # window = Tk()
-    		# Window(window)
+            window = Tk()
+            Window(window)
+
         else:
             ms.showerror('Oops!','Username Not Found.')
             # showerror(title = "warning", message = "incorrect username or password")
-    	# Connect to database
 
 
     def registration(self):
@@ -80,12 +75,7 @@ class main:
 
 
         self.head = Label(self.master,text = 'Start page',font = ('',35),pady = 10)
-
         self.head.pack()
-        # self.head1 = Frame(self.master,padx=0,pady=0)
-        # self.head1.pack()
-
-
         self.logf = Frame(self.master,padx =10,pady = 10)
         Label(self.logf,text = 'Username: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
         Entry(self.logf,textvariable = self.username,bd = 3,font = ('',15)).grid(row=0,column=1)
@@ -93,61 +83,26 @@ class main:
         Entry(self.logf,textvariable = self.password,bd = 3,font = ('',15),show = '*').grid(row=1,column=1)
         Button(self.logf,text = ' Login ',bd = 3 ,borderwidth=3,width = 15,padx=5,pady=5,command=self.login).grid()
         Button(self.logf,text = ' Sign Up ',bd = 3 ,borderwidth=3,width = 15,padx=5,pady=5,command=self.cr).grid(row=2,column=1)
-        # tkinter.Button(self, text="SignUp",borderwidth=3, relief='ridge', fg=fg_color, bg=bg_color, width = 15, command = register).grid(row = 10,column=1,  padx=(50, 0), pady=(20, 10))
 
         self.logf.pack()
 
         self.crf = Frame(self.master,padx =10,pady = 10)
         Label(self.crf,text = 'Username: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
-        Entry(self.crf,textvariable = self.n_username,bd = 5,font = ('',15)).grid(row=0,column=1)
+        Entry(self.crf,textvariable = self.n_username,bd =3,font = ('',15)).grid(row=0,column=1)
         Label(self.crf,text = 'Password: ',font = ('',20),pady=5,padx=5).grid(sticky = W)
-        Entry(self.crf,textvariable = self.n_password,bd = 5,font = ('',15),show = '*').grid(row=1,column=1)
-        Button(self.crf,text = 'Create Account',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.registration).grid()
-        Button(self.crf,text = 'Go to Login',bd = 3 ,font = ('',15),padx=5,pady=5,command=self.log).grid(row=2,column=1)
+        Entry(self.crf,textvariable = self.n_password,bd =3,font = ('',15),show = '*').grid(row=1,column=1)
+        Button(self.crf,text = 'Create Account',borderwidth=3,width = 15,padx=5,pady=5,command=self.registration).grid()
+        Button(self.crf,text = 'Go to Login',borderwidth=3,width = 15,padx=5,pady=5,command=self.log).grid(row=2,column=1)
 
-    	# bg_color = "white"
-    	# fg_color = "#383a39"
-    	# # -------username
-        #
-    	# # ----password
-    	# tkinter.Label(self,  text="Password:", fg=fg_color, bg=bg_color, font=("Helvetica", 15)).grid(row=9, padx=(50, 0), pady=(20, 10))
-    	# --------button
-    	# tkinter.Button(self, text="Login",borderwidth=3, relief='ridge', fg=fg_color, bg=bg_color, width = 15, command = login).grid(row = 10,  padx=(50, 0), pady=(20, 10))
 
 if __name__ == "__main__":
-    # root =tkinter.Tk()
-    # app = Main(root)
-    # app.pack()
-
-    #Create Object
-	#and setup window
     root = Tk()
-    # root.title('Login Form')
-
     root.title("Hof Library")
     root.geometry("450x350")
     img = ImageTk.PhotoImage(Image.open("library4.png"))
     panel = Label(root, image = img)
     panel.pack(side = "top")
-
     #root.geometry('400x350+300+300')
     main(root)
     root.mainloop()
-
-    # root.geometry("450x350")
     # root.resizable(False, False)
-
-# label1=Label(f1,text="Name of book")
-# label1.grid(row=0,column=0)
-
-# label2=Label(f1,text="Author")
-# label2.grid(row=1,column=0)
-
-# label3=Label(f1,text="Library3")
-# label3.grid(row=2,column=0)
-
-# label4=Label(f1,text="Library4")
-# label4.grid(row=3,column=0)
-
-# label5=Label(f1,text="Library5")
-# label5.grid(row=4,column=0)
